d181f2a27aea9aba1fa9310d2387fbf40d324b81
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-05-25 20:03:58 -0500

    avance nodejs esencial
    ejercicios 2-6

---
+++ backend/nodejs_esencial/ej3_datos_primitivos/app.js
@@ -0,0 +1,5 @@
+var nombre = "Ana Karent";
+
+    nombre = true;
+
+    console.log(nombre)
---
+++ backend/nodejs_esencial/ej6_buffer-process/app.js
@@ -0,0 +1,5 @@
+
+console.log(Buffer(5));
+
+process.env.colorFavorito = "rojo"
+console.log( process.env.colorFavorito);

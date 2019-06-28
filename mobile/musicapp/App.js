1e8aed5c3a4aa8cd094c96bd4149f9d29a0471b9
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-06-28 15:21:14 -0500

    avance mobile
    parte inicial musicapp

--- mobile/musicapp/App.js
+++ mobile/musicapp/App.js
@@ -7,23 +7,34 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    const image = 'https://areajugones.sport.es/wp-content/uploads/2018/03/DXVhKJtVQAAx-Jd-810x400.jpg';
+    const name = 'El kokun ';
+    const likes = 999999999;
+    const comentarios = 99999999999999;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Que pedo!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.artistBox}>
+          <Image style={styles.image} source={{ uri: image }}></Image>
+          <View style={styles.info}>
+            <Text style={styles.name}>{name}</Text>
+            <View style={styles.row}>
+              <View style={styles.iconContainer}>
+                <Icon name="heart" size={30} color="lightgray" />
+                <Text style={styles.count}>{likes}</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Icon name="comment" size={30} color="lightgray" />
+                <Text style={styles.count}>{comentarios}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -32,18 +43,40 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'lightgray',
+    paddingTop: 50
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  artistBox: {
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  info: {
+    flex: 1,
     justifyContent: 'center',
+    flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  name: {
+    fontSize: 30,
+    marginTop: 10,
+    color: '#333'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 35,
+    marginTop: 10
   },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: 'gray'
+  }
 });

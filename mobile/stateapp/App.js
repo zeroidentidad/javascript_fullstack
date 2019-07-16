a3bc3d9c027bc3ef7eca2e6ab7a824d61c516c11
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-07-16 11:39:07 -0500

    avance mobile
    ej uso estado

--- mobile/stateapp/App.js
+++ mobile/stateapp/App.js
@@ -7,23 +7,40 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state = {
+      saludo: 'Hola prros del mal',
+      user: 256,
+      userName: 'x'
+    }
+  }
+
+  sumarUsuario = () => {
+    this.setState({
+      user: this.state.user+1
+    })
+  }  
+
   render() {
+
+    let {saludo} = this.state
+    let {user} = this.state
+    let {userName} = this.state
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={{fontSize:24, padding:10}}>{saludo}, Usuario: {user}</Text>
+        <Text style={{ fontSize: 24, padding: 10 }}>User Name: {userName}</Text>
+        <TextInput 
+        style={{ fontSize: 24, padding: 10 }}
+          onChangeText={(userName) => this.setState({ userName })}
+        >{userName}</TextInput>
+        <Button style={{ padding: 10 }} title="Sumar usuario" onPress={this.sumarUsuario}></Button>
       </View>
     );
   }
@@ -35,15 +52,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });

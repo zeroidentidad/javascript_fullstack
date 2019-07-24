521d554237424527f3687346a4e5ccfea9f5e254
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-07-24 13:06:46 -0500

    av js mod
    variables, event listeners y funciones seleccionar e insertar

--- frontend/javascript-es6-es7_con_proyectos/simular_carritoCompra/js/app.js
+++ frontend/javascript-es6-es7_con_proyectos/simular_carritoCompra/js/app.js
@@ -1,7 +1,49 @@
 //Variables
-
+const carrito = document.getElementById('carrito');
+const cursos = document.getElementById('lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody');
 
 //Listeners
+cargarEventListeners();
 
+function cargarEventListeners() {
+    //Cuando de presiona agregar carrito
+    cursos.addEventListener('click', comprarCurso)
+}
 
 //Funciones
+
+function comprarCurso(e) {
+    e.preventDefault();
+
+    //Delegation agregar carrito
+    if(e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement
+        //Enviar datos card elemento seleccionado
+        leerDatosCurso(curso);
+    }
+}
+
+function leerDatosCurso(curso){
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
+    
+    insertarCarrito(infoCurso);
+}
+
+//Mostrar curso seleccionado
+function insertarCarrito(infoCurso){
+    const row = document.createElement('tr');
+    row.innerHTML=`
+    <td><img width="100" src="${infoCurso.imagen}" /></td>
+    <td>${infoCurso.titulo}</td>
+    <td>${infoCurso.precio}</td>
+    <td><a href="#" class="borrar-curso" data-id="${infoCurso.id}">X<a/></td>
+    `;
+
+    listaCursos.appendChild(row);
+}

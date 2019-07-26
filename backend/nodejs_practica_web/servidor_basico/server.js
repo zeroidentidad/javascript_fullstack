40f2ce0b81d19527aa2eedaca5d523aefb978b3f
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-07-25 21:30:53 -0500

    av nodejs web
    ip test

--- backend/nodejs_practica_web/servidor_basico/server.js
+++ backend/nodejs_practica_web/servidor_basico/server.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("Funcionando :D epa");
+    res.write("Desde Node en Linux");
     res.end();
-}).listen(3000, 'localhost');
+}).listen(3000, '172.30.109.95');
 
 console.log('Servidor iniciado')

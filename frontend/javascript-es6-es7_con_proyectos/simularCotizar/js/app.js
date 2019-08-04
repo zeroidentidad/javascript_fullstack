d4ea42b8ec13e04e059a6e313495e3351d4119c6
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-04 12:11:44 -0500

    av js mod
    proceso de cotización en la interfaz

--- frontend/javascript-es6-es7_con_proyectos/simularCotizar/js/app.js
+++ frontend/javascript-es6-es7_con_proyectos/simularCotizar/js/app.js
@@ -1,12 +1,71 @@
 //Cotizador constructor
 function Seguro(marca, anio, tipo) {
-    this.marca= marca;
+    this.marca = marca;
     this.anio = anio;
     this.tipo = tipo;
 }
+Seguro.prototype.cotizarSeguro = function () {
+    //console.log(info);
+    /* Sobre:
+        1 = americano -> 1.15, 2 = asiatico -> 1.05, 3 = europeo -> 1.35
+    */
+     //Precio base:
+     const base = 2500;
+     let cantidad;
+     switch (this.marca) {
+         case '1':
+             cantidad = base * 1.15
+             break;
+         case '2':
+             cantidad = base * 1.05
+             break;
+         case '3':
+             cantidad = base * 1.35
+             break;                  
+         default:
+             break;
+     }
+
+     //Usar año:
+     const diferencia = new Date().getFullYear()-this.anio;
+     //Cada año de diferencia se reducira 2.5% el valor del seguro:
+     cantidad -= ((diferencia*2.5)*cantidad)/100;
+
+     //Usar tipo:
+     /* Info:
+        Basico se multiplica por 30%
+        Completo se multipĺlica por 50% mas
+     */
+    if(this.tipo=='basico'){
+        cantidad *= 1.30;
+    } else if (this.tipo=='completo'){
+        cantidad *= 1.50;
+    }
+
+    //console.log(cantidad);
+
+    return cantidad;
+}
 
 //Toda la interaccion en el DOM
-function Interfaz() {
+function Interfaz() {}
+
+Interfaz.prototype.mostrarMsge = function (mensaje, tipo) {
+    const div = document.createElement('div');
+
+    if (tipo =='error') {
+        //div.classList = 'error';
+        div.classList.add('mensaje', 'error');
+    } else{
+        div.classList.add('mensaje', 'correcto');;
+    }
+    div.innerHTML = `${mensaje}`;
+    formulario.insertBefore(div, document.querySelector('.form-group'));
+
+    setTimeout(() => {
+        //div.remove();
+        document.querySelector('.mensaje').remove();
+    }, 2500);
 }
 
 //Event listeners
@@ -27,10 +86,14 @@ formulario.addEventListener('submit', function (e) {
 
     //Revisar campos:
     if (marcaSelect === ''||anioSelect === ''||tipo === '' ){
-        console.log('faltan datos');
+        interfaz.mostrarMsge('Faltan datos, revisar formulario. Reintentar.', 'error');
     }else{
         //Instanciar seguro:
-        console.log('correcto');
+        const seguro = new Seguro(marcaSelect, anioSelect, tipo);
+        //console.log(seguro);
+        
+        //Cotizar algorithm call
+        const cantidad = seguro.cotizarSeguro(seguro);
     }
 });
 

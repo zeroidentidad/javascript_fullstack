7b95aa33716d4f2d429d6c216fc468cf1e185807
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-04 13:03:34 -0500

    av js mod
    final version con base en prototypes

--- frontend/javascript-es6-es7_con_proyectos/simularCotizar/js/app.js
+++ frontend/javascript-es6-es7_con_proyectos/simularCotizar/js/app.js
@@ -49,7 +49,6 @@ Seguro.prototype.cotizarSeguro = function () {
 
 //Toda la interaccion en el DOM
 function Interfaz() {}
-
 Interfaz.prototype.mostrarMsge = function (mensaje, tipo) {
     const div = document.createElement('div');
 
@@ -66,6 +65,41 @@ Interfaz.prototype.mostrarMsge = function (mensaje, tipo) {
         //div.remove();
         document.querySelector('.mensaje').remove();
     }, 2500);
+}
+Interfaz.prototype.mostrarResultado = function (seguro, cantidad) {
+    const resultado = document.getElementById('resultado');
+    let marca;
+    switch (seguro.marca) {
+        case '1':
+            marca ='Americano';
+            break;
+        case '2':
+            marca = 'Asiatico';
+            break;
+        case '3':
+            marca = 'Europeo';
+            break;                
+        default:
+            break;
+    }
+    //Div resultado
+    const div = document.createElement('div');
+    div.innerHTML = 
+    `<p class='header'><u>Resumen:</u></p>
+     <p>Marca: <b>${marca}</b></p>
+     <p>Año: <b>${seguro.anio}</b></p>
+     <p>Tipo: <b>${seguro.tipo}</b></p>
+     <p><u>Total:<u> <b>${cantidad}</b></p>
+    `;
+
+    const spinner = document.querySelector('#cargando img');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        //spinner.remove();
+        resultado.appendChild(div);
+    }, 2350);
 }
 
 //Event listeners
@@ -88,12 +122,21 @@ formulario.addEventListener('submit', function (e) {
     if (marcaSelect === ''||anioSelect === ''||tipo === '' ){
         interfaz.mostrarMsge('Faltan datos, revisar formulario. Reintentar.', 'error');
     }else{
+        //Limpiar anterior:
+        const resultados = document.querySelector('#resultado div');
+        if(resultados!=null){
+            resultados.remove();
+        }
+
         //Instanciar seguro:
         const seguro = new Seguro(marcaSelect, anioSelect, tipo);
         //console.log(seguro);
         
         //Cotizar algorithm call
-        const cantidad = seguro.cotizarSeguro(seguro);
+        const cantidad = seguro.cotizarSeguro();
+        //Mostrar resultado
+        interfaz.mostrarResultado(seguro, cantidad);
+        interfaz.mostrarMsge('Cotizando...', 'correcto');
     }
 });
 

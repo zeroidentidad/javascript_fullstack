1e0db7ad46f4f41037ad17c1ce31b300fdf5c2a5
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-10 16:08:24 -0500

    av js mod
    interaccion msjes formulario error

--- frontend/javascript-es6-es7_con_proyectos/simularGastoSem/js/app.js
+++ frontend/javascript-es6-es7_con_proyectos/simularGastoSem/js/app.js
@@ -1,5 +1,7 @@
 //Variables
 const presupuestoUsuario = document.getElementById('presupuestoInicial');
+const formulario = document.getElementById('agregar-gasto');
+
 let cantidadPresupuesto;
 
 //Clases
@@ -24,10 +26,28 @@ class Interfaz {
         presupuestoSpan.innerHTML = `${cantidad}`;
         restanteSpan.innerHTML = `${cantidad}`;
     }
+
+    imprimirMsje(mensaje, tipo){
+        const divMsje = document.createElement('div');
+        divMsje.classList.add('text-center', 'alert');
+        if (tipo==='error') {
+            divMsje.classList.add('alert-danger')
+        }else{
+            divMsje.classList.add('alert-success')     
+        }
+        divMsje.appendChild(document.createTextNode(mensaje));
+        document.querySelector('.primario').insertBefore(divMsje, formulario);
+
+        // quitar alert despues de 2.5 seg
+        setTimeout(() => {
+            document.querySelector('.primario .alert').remove()
+            formulario.reset()
+        }, 2500);
+    }
 }
 
 //Event Listeners
-presupuestoUsuario.addEventListener('blur', function () {
+presupuestoUsuario.addEventListener('blur', function (){
     if (presupuestoUsuario.value === null || presupuestoUsuario.value ===''){
         alert('Agrega presupuesto inicial');
     } else {
@@ -35,5 +55,22 @@ presupuestoUsuario.addEventListener('blur', function () {
         //instanciar interfaz
         const ui = new Interfaz();
         ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
+    }
+})
+
+formulario.addEventListener('submit', function (e){
+    e.preventDefault();
+
+    //Leer datos gastos:
+    const nombreGasto = document.getElementById('gasto').value;
+    const cantidadGasto = document.getElementById('cantidad').value;
+
+    //instanciar interfaz
+    const ui = new Interfaz();
+    if (nombreGasto === '' || cantidadGasto === '') {
+        // mensaje y tipo:
+        ui.imprimirMsje('Ocurrio un error', 'error')
+    } else {
+    ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
     }
 })

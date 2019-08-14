43ce673ce0b79b1782aa900f8223dbd3e28b34c1
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-14 02:33:09 -0500

    electron app
    avance

--- x-electron-app/main.js
+++ x-electron-app/main.js
@@ -17,10 +17,11 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('./app/index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // descomentado
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -51,3 +52,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+//Habilitar live reload
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+})

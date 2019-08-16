f0a51222ae7584658d90e37420717dc5e66ab36c
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-16 11:41:38 -0500

    ajax-json
    json personas

--- frontend/javascript-es6-es7_con_proyectos/temaAjax-JSON/AJAXJSON/app.js
+++ frontend/javascript-es6-es7_con_proyectos/temaAjax-JSON/AJAXJSON/app.js
@@ -25,3 +25,35 @@ boton1.addEventListener('click', function() {
 
     xhr.send();
 })
+
+const boton2 = document.getElementById('boton2');
+
+boton2.addEventListener('click', function () {
+    xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'empleados.json', true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const personas = JSON.parse(this.responseText);
+
+            let html = ``;
+            personas.forEach(persona => {
+
+            html += `
+            <ul>
+                <li>ID: ${persona.id}</li>
+                <li>Nombre: ${persona.nombre}</li>
+                <li>Empresa: ${persona.empresa}</li>
+                <li>Trabajo: ${persona.trabajo}</li>
+            </ul>
+            `;
+                           
+            });
+
+            document.getElementById("empleados").innerHTML = html; 
+        }
+    }
+
+    xhr.send();
+})

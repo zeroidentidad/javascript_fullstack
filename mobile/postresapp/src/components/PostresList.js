23d6e36bc5ceecbf01cb3075edb9fa5be4ca1680
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-22 23:04:04 -0500

    react native esencial
    uso flatlist

--- mobile/postresapp/src/components/PostresList.js
+++ mobile/postresapp/src/components/PostresList.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import PropTypes from 'prop-types';
 
 export default class PostresList extends Component {
+
+    static propTypes = {
+        postres: PropTypes.array.isRequired
+    }
+
     render() {
         return (
-            <View>
-                <Text> PostresList </Text>
+            <View style={styles.list}>
+                {/*this.props.postres.map((postre) => <Text key={postre.key}> {postre.title} </Text>)*/}
+                <FlatList
+                    data={this.props.postres}
+                    renderItem={({ item }) => <Text> {item.title} </Text> }
+                />                
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        backgroundColor: '#eee',
+        width: '100%',
+        paddingTop: 45
+    }
+});
+

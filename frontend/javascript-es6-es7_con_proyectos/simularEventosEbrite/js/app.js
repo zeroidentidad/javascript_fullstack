06be5fead866b930492e3109e93ba2a096a8449f
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-01 14:20:20 -0500

    av js mod
    comportamiento buscador

--- frontend/javascript-es6-es7_con_proyectos/simularEventosEbrite/js/app.js
+++ frontend/javascript-es6-es7_con_proyectos/simularEventosEbrite/js/app.js
@@ -4,4 +4,13 @@ const interfaz = new Interfaz();
 //Listeners:
 buscarBtn.addEventListener('click', (e)=>{
     e.preventDefault();
+
+    const categoriaSeleccionada = listadoCategorias.options[listadoCategorias.selectedIndex].value;
+
+    //revisar valor en campo de texto del buscador:
+    if (evento.value !== ''){
+        console.log('Buscando...')
+    }else{
+        console.log('Nada...')
+    }
 })

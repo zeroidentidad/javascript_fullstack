3f475e7d8cf5a9d27ed93cb2a9c5fb6fd9c4ca0b
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-02 23:59:09 -0500

    avance mobile
    uso de rename app para nombre interno y del bundle

--- mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainActivity.java
+++ mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.regalosapp;
+package com.zeroapps.regalosapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "regalosapp";
+        return "Intercambios";
     }
 }
--- mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainApplication.java
+++ mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.regalosapp;
+package com.zeroapps.regalosapp;
 
 import android.app.Application;
 

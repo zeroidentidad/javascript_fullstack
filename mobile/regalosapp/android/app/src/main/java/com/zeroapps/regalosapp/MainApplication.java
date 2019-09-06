525a32c7022e4f12319fa5afd3f4fda4e423b9f1
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-06 00:41:34 -0500

    av js mod
    configuracion firebase

--- mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainApplication.java
+++ mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactNativeFirebaseAppPackage(),
+          new ReactNativeFirebaseAuthPackage()
       );
     }
 

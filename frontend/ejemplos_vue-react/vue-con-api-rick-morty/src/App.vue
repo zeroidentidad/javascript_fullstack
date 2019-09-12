f91862bc7d816ad2903b85bffe915ddb79aa4853
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-12 00:58:59 -0500

    ej vue app
    avance manejo components

--- frontend/ejemplos_vue-react/vue-con-api-rick-morty/src/App.vue
+++ frontend/ejemplos_vue-react/vue-con-api-rick-morty/src/App.vue
@@ -1,11 +1,56 @@
 <template>
-  <div id="app">
-    <h1 class="title">Hola mundo</h1>
+  <div id="App">
+
+    <div class="hero is-white is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">Rick and Morty</span>
+          <span class="subtitle">Personajes</span>
+        </h1>
+        <button class="button is-warning is-rounded" v-on:click="fetch">Consultar</button>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+
+        <personaje v-for="character of characters" v-bind:key="character.id" v-bind:character="character"/>
+
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Personaje from './components/Personaje'
+
 export default {
-  name: 'app'
+  name: 'App',
+  components:{
+    Personaje
+  },
+  data: function () {
+    return {
+      characters: []
+    }
+  },
+  created(){
+    this.fetch()
+  },  
+  methods: {
+    fetch(){
+      let result = axios.get("https://rickandmortyapi.com/api/character")
+      .then((res)=>{
+        this.characters = res.data.results;
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    }
+  }
 }
 </script>

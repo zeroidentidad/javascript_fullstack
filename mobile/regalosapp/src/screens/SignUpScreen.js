f22d40767ac64d1690c5a68ce92ecf5faf2a457e
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-14 14:27:44 -0500

    avance mobile
    agregar usuario a db firestore

--- mobile/regalosapp/src/screens/SignUpScreen.js
+++ mobile/regalosapp/src/screens/SignUpScreen.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import AuthUI from '../components/AuthUI';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import {connect} from 'react-redux';
 import {login} from '../actions/user';
 
 class SignUpScreen extends Component {
     constructor(props){
         super(props);
+        this.db = firestore();
     }
 
     createUser = async({email, password}) => {
@@ -14,6 +16,7 @@ class SignUpScreen extends Component {
            let res = await auth().createUserWithEmailAndPassword(email, password);
            let { user } = res;
            //console.warn(user);
+           await this.db.collection('users').doc(user.uid).set({email: user.email});
            this.props.login(user);          
        } catch (err) {
            alert(err);

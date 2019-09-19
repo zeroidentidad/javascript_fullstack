4493a8c17aa4ec1329f276e412fa93c79baf3008
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-19 12:06:54 -0500

    ej vue app
    avance pantalla

--- frontend/ejemplos_vue-react/vue-realtime-firestore/src/components/Sala.vue
+++ frontend/ejemplos_vue-react/vue-realtime-firestore/src/components/Sala.vue
@@ -1,19 +1,54 @@
 <template lang="html">
     <div class="container">
-        <div class="row">
-            <div class="column">
-                <h2>Componente Sala</h2>
-            </div>
+        <div class="pantalla">
+            <p class="font-weight-bold textoInicial">Pantalla</p>
         </div>
+        <div class="asientos">
+            <div class="row">
+                <div class="col asiento disponible" 
+                v-for="asiento in asientos" 
+                v-text="asiento.id">
+                </div>
+            </div>
+        </div>        
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            asientos:[
+                { id:'A1', disponible:true },
+                { id:'A2', disponible:true },
+                { id:'A3', disponible:true },
+                { id:'A4', disponible:true }
+            ]
+        }
+    }  
 }
 </script>
 
 <style lang="css" scoped>
-
+.pantalla{
+    background-color: #2b6282;
+}
+.textoInicial{
+  color: bisque;
+  font-size: 20px;  
+}
+.asientos{
+    margin-top: 50px;
+}
+.asiento{
+    color: white;
+    margin: 4px;
+    font-weight: bolder;
+}
+.disponible{
+    background-color: #2d4d86
+}
+.ocupado{
+    background-color: #73264f
+}
 </style>

e4b574b8c1f70517189cb9e1e601ead78803eb8c
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-18 22:53:58 -0500

    ej vue app
    config inicial

--- frontend/ejemplos_vue-react/vue-realtime-firestore/src/App.vue
+++ frontend/ejemplos_vue-react/vue-realtime-firestore/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>  
+    <Sala/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sala from './components/Sala'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sala
   }
 }
 </script>
---
+++ frontend/ejemplos_vue-react/vue-realtime-firestore/src/components/Sala.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <h2>Componente Sala</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>

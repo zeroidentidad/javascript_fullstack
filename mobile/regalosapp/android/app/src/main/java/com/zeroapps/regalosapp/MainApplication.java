84a8625b257f8ae52180da2188098be126890e75
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-25 14:25:53 -0500

    avance mobile
    dependencia usar contactos

--- mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainApplication.java
+++ mobile/regalosapp/android/app/src/main/java/com/zeroapps/regalosapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.zeroapps.regalosapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import io.invertase.firebase.firestore.ReactNativeFirebaseFirestorePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new ReactNativeContacts(),
           new ReactNativeFirebaseFirestorePackage(),
           new VectorIconsPackage(),
           new RNGestureHandlerPackage(),

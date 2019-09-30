d540764683fc31545c1791cb71dd5f8b5d61cbea
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-09-30 02:08:17 -0500

    ej react website
    react router web de ejemplo

---
+++ frontend/ejemplos_vue-react/react-router-website/src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { MemoryRouter } from 'react-router-dom';
+import Vitamin from './components/Vitamin';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Vitamin renders successfully', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MemoryRouter initialEntries={['/vitamin']}>
+      <Vitamin />
+    </MemoryRouter>
+    , div);
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ frontend/ejemplos_vue-react/react-router-website/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+const Navigation = (props) => (
+  <div id="myNav" className="overlay">
+    <a className="closebtn" onClick={() => props.closeNav()}>&times;</a>
+    <div className="overlay-content">
+      <Link to="/" onClick={() => props.closeNav()}>Inicio</Link>
+      <Link to="/vitamin" onClick={() => props.closeNav()}>Jugo de vitamina</Link>
+      <a href="">Ropa</a>
+      <a href="">Suplementos</a>
+      <a href="">Contacto</a>
+    </div>
+  </div>
+);
+
+export default Navigation;

f2539dd935e33faa02f6e201393cf8c388389799
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-02 20:10:06 -0500

    ej vue app
    fin parte tema componentes basico

--- frontend/ejemplos_vue-react/vue-esencial-app/src/components/MiComponente.vue
+++ frontend/ejemplos_vue-react/vue-esencial-app/src/components/MiComponente.vue
@@ -3,6 +3,9 @@
         <h1>Libro</h1>
         <h2>Autor: {{libro.autor}}</h2>
         <h2>Titulo: {{libro.titulo}}</h2>
+        <h2>Juntos: {{`${libro.titulo} ${libro.autor.toString().toUpperCase()}`}}</h2>
+        <h2>Suma: {{ 10 + 20 }}</h2>
+        <div v-html="libro.texto"></div>
     </div>
 </template>
 
@@ -13,7 +16,8 @@ export default {
         return {
             libro:{
                 autor: "Alguien",
-                titulo: "Teoria de Sistemas"
+                titulo: "Teoria de Sistemas",
+                texto: "<h1>Lorem ipsum</h1> <i>inadolor sape</i>"
             }
         }
     }

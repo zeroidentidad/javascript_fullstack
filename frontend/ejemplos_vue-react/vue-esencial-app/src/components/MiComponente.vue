d6506d3363761f70a1f5a084b9db5345900c939c
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-02 19:34:26 -0500

    ej vue app
    proyecto curso vue esencial y avance tema componentes

---
+++ frontend/ejemplos_vue-react/vue-esencial-app/src/components/MiComponente.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="MiComponente">
+        <h1>Libro</h1>
+        <h2>Autor: {{libro.autor}}</h2>
+        <h2>Titulo: {{libro.titulo}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data (){
+        return {
+            libro:{
+                autor: "Alguien",
+                titulo: "Teoria de Sistemas"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>

e0314cc3db783734740463b45b573fc5d80ce628
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-07 17:54:42 -0500

    ej vue app
    manejo estilos

--- frontend/ejemplos_vue-react/vue-esencial-app/src/App.vue
+++ frontend/ejemplos_vue-react/vue-esencial-app/src/App.vue
@@ -2,19 +2,32 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">-->  
     <MiComponente />
-    <HelloWorld v-for="x in 5" v-bind:key="x" msg="Hola Mundo"/>  
+    <HelloWorld v-for="x in 5" v-bind:key="x" msg="Hola Mundo"/>
+
+    <div class="container">
+      <div class="row">
+        <Nav />        
+      </div>
+      <div class="row">
+        <h1>Website</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus earum nulla incidunt nemo facere possimus saepe dolorem fugiat culpa nisi optio omnis, voluptate ipsa debitis itaque! Culpa alias facilis reiciendis.</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import MiComponente from './components/MiComponente.vue'
+import Nav from './components/Nav.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    MiComponente
+    MiComponente,
+    Nav
   }
 }
 </script>
@@ -24,8 +37,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: justify;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 0 10% 0 10%;
 }
 </style>
---
+++ frontend/ejemplos_vue-react/vue-esencial-app/src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" v-bind:class="{active: mostrarActivo}" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>    
+</template>
+
+<script>
+//opcional
+export default {
+  name: 'Nav',
+  data() {
+      return {
+          mostrarActivo: true
+      }
+  }
+}
+</script>

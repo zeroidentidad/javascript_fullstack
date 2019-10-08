9a842a766779b748fbe02fe9aac4dbb637ebc7e0
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-08 12:49:53 -0500

    ej vue app
    manejo componentes, anidacion

--- frontend/ejemplos_vue-react/vue-esencial-app/src/components/Nav.vue
+++ frontend/ejemplos_vue-react/vue-esencial-app/src/components/Nav.vue
@@ -1,8 +1,7 @@
 <template>
       <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link" v-bind:class="{active: mostrarActivo}" href="#">Active</a>
-        </li>
+        <NavItem />
+        <NavItem />
         <li class="nav-item" v-bind:style="{backgroundColor: colorRand, border: 'solid 3px red'}">
           <a class="nav-link" href="#">Link</a>
         </li>
@@ -16,12 +15,16 @@
 </template>
 
 <script>
+import NavItem from './NavItem.vue'
+
 //opcional
 export default {
   name: 'Nav',
+  components:{
+    NavItem
+  },
   data() {
       return {
-          mostrarActivo: true,
           colorRand: '#'+Math.floor(Math.random()*6777215).toString(16)
       }
   }
@@ -29,7 +32,5 @@ export default {
 </script>
 
 <style>
-.active {
-    font-weight: bold;
-}
+
 </style>
---
+++ frontend/ejemplos_vue-react/vue-esencial-app/src/components/NavItem.vue
@@ -0,0 +1,23 @@
+<template>
+        <li class="nav-item">
+          <a class="nav-link" v-bind:class="{active: mostrarActivo}" href="#">Active</a>
+        </li>  
+</template>
+
+<script>
+
+export default {
+    name: "NavItem",
+    data() {
+      return {
+          mostrarActivo: true
+      }
+    }
+}
+</script>
+
+<style>
+.active {
+    font-weight: bold;
+}
+</style>

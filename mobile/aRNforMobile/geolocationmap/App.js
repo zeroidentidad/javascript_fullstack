4eec61fa3b9d0b415d14a6ac943cac5127e318f0
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-10 02:27:49 -0500

    RN for mobile
    test maps

--- mobile/aRNforMobile/geolocationmap/App.js
+++ mobile/aRNforMobile/geolocationmap/App.js
@@ -1,32 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       region: {
-        latitude: 37.3230,
-        longitude: -122.0322,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0922,
+        latitude: 17.84148,
+        longitude: -93.2357091,
+        latitudeDelta: 0.0999,
+        longitudeDelta: 0.0999,
       },
       coordinate: {
-        latitude: 37.3230,
-        longitude: -122.0322,
+        latitude: 17.84148,
+        longitude: -93.2357091,
       },
     };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.watchPosition(
+      (position) => {
+        //console.log(position);
+        this.map.animateToRegion({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005
+        });
+      },
+      (error) => console.log(error.message),
+      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+    );
   }
 
   render() {
     return (
       <MapView
+        ref={ref => { this.map = ref; }}
         style={styles.container}
-        provider="google"
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        followUserLocation={true}
+        loadingEnabled={true}
         initialRegion={this.state.region}
       >
-        <Marker coordinate={this.state.coordinate} />
+        <Marker
+        title="Marcador emulado"
+        description="textto descriptivo"
+        coordinate={this.state.coordinate}
+        />
       </MapView>
     );
   }

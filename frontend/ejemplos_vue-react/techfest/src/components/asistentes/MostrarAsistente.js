5fbd71212c2c5c954a640cc2ae86226a472bd883
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-18 23:13:17 -0500

    techfest
    mostrar individual

--- frontend/ejemplos_vue-react/techfest/src/components/asistentes/MostrarAsistente.js
+++ frontend/ejemplos_vue-react/techfest/src/components/asistentes/MostrarAsistente.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
 
-export default function MostrarAsistente() {
+const MostrarAsistente = ({ asistente }) => {
+    if (!asistente) return <Spinner />
+
     return (
-        <h1>Asistente</h1>
-    )
+        <div className="row">
+            <div className="col-md-6 mb-4">
+                <Link to="/asistentes" className="btn btn-secondary">
+                    <i className="fas fa-arrow-circle-left"></i> Regresar al listado
+                </Link>
+            </div>
+            <div className="col-md-6">
+                <Link to={`/asistentes/editar/${asistente.id}`} className="btn btn-primary float-right">
+                    <i className="fas fa-pencil-alt"></i> Editar asistente
+                </Link>
+            </div>
+
+            <hr className="mx-5 w-100" />
+
+            <div className="col-12">
+                <h2 className="mb-4">
+                    {asistente.nombre} {asistente.apellido}
+                </h2>
+
+                <p>
+                    <span className="font-weight-bold">
+                        Profesión:
+                    </span>{' '} 
+                    {asistente.profesion}
+                </p>
+                <p>
+                    <span className="font-weight-bold">
+                        Matricula:
+                    </span>{' '} 
+                    {asistente.matricula}
+                </p>
+                <p>
+                    <span className="font-weight-bold">
+                        Nick redes sociales:
+                    </span>{' '}
+                    {asistente.nickname}
+                </p>                
+            </div>
+        </div>
+    );
 }
+
+MostrarAsistente.propTypes = {
+    firestore: PropTypes.object.isRequired
+}
+
+export default compose(
+    firestoreConnect(props => [
+        {
+            collection: 'asistentes',
+            storeAs: 'asistente',
+            doc: props.match.params.id
+        }
+    ]),
+    connect(({ firestore: { ordered } }, props) => ({
+        asistente: ordered.asistente && ordered.asistente[0]
+    }))
+)(MostrarAsistente)

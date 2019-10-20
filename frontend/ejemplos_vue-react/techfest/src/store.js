7acf43d32368c4dc29ba58b7a322167b6a3133bf
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-20 00:03:15 -0500

    techfest
    deploy heroku

--- frontend/ejemplos_vue-react/techfest/src/store.js
+++ frontend/ejemplos_vue-react/techfest/src/store.js
@@ -45,7 +45,7 @@ const initialState = {};
 
 //Crear store
 const store = createStoreWithFirebase(rootReducer, initialState, compose(
-    reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reactReduxFirebase(firebase)/*,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
 ));
 export default store;

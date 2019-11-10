3f29bcb991bf351f944a2632b4b64fe7d744c7c5
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-10 12:02:07 -0600

    ej vue
    con cli forma single file component

---
+++ frontend/ejemplos_vue-react/vue-practico-sin-cli/x-componentes/single-file-components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Ejemplo />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Ejemplo from './components/Ejemplo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Ejemplo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

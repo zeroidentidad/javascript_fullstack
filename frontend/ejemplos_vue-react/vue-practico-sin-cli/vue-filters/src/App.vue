7c68033f6efdda4809f4f9a71dd058afe86b73a4
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-16 14:02:10 -0600

    ej vue
    vue filters

---
+++ frontend/ejemplos_vue-react/vue-practico-sin-cli/vue-filters/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Componente></Componente>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Componente from './components/Componente.vue'
+
+export default {
+  name: 'app',
+  components: {
+    //HelloWorld
+    Componente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/ejemplos_vue-react/vue-practico-sin-cli/vue-filters/src/components/Componente.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="text" v-model="name">
+        <p>{{ name | uppercase}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name: ""
+        }
+    },
+    components: {
+
+    },
+    filters: {
+        uppercase: function(value){
+            if(!value) return '';
+            return value.toString().toUpperCase();
+        },
+        lowercase: function(value){
+            if(!value) return '';
+            return value.toString().toLowerCase();
+        }        
+    }
+}
+</script>
+
+<style>
+
+</style>

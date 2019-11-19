56e9d492c2f2d458350a7f1ead938b22cfd24c7f
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-19 17:04:30 -0600

    ej vue
    agregar vuex

--- frontend/ejemplos_vue-react/vue-practico-sin-cli/vue-router/src/main.js
+++ frontend/ejemplos_vue-react/vue-practico-sin-cli/vue-router/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import VueRouter from 'vue-router'
+import store from './store.js'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')

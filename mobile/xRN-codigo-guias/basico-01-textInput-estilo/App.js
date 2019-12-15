42d9d81a2435220a508c793bf5fcc801b747fe70
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-12-15 16:23:34 -0600

    rn codigo guias
    ej codigo basico textinput

---
+++ mobile/xRN-codigo-guias/basico-01-textInput-estilo/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+export default class App extends Component {
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{width: 300}}
+          placeholder="Gran dia papu"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
+});

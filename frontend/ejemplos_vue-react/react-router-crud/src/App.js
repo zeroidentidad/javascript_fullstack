ffcb42b2e5d394ecdff842185d50af498fa8c92e
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-12 18:00:55 -0600

    ej react app
    nav header

--- frontend/ejemplos_vue-react/react-router-crud/src/App.js
+++ frontend/ejemplos_vue-react/react-router-crud/src/App.js
@@ -4,16 +4,20 @@ import Productos from './components/Productos'
 import Producto from './components/Producto'
 import AgregarProducto from './components/AgregarProducto'
 import EditarProducto from './components/EditarProducto'
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
+      <Header />
+      <main className="container mt-4">
       <Switch>
         <Route exact path="/nuevo-producto" component={AgregarProducto} />
         <Route exact path="/productos" component={Productos} />
         <Route exact path="/productos/:id" component={Producto} />
         <Route exact path="/productos/editar/:id" component={EditarProducto} />
       </Switch>
+      </main>
     </Router>
   );
 }

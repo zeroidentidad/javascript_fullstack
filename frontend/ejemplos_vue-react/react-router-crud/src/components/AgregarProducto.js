783199126df97c491e993892103949991c238981
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-12 20:17:56 -0600

    ej react app
    formulario agregar nuevo

--- frontend/ejemplos_vue-react/react-router-crud/src/components/AgregarProducto.js
+++ frontend/ejemplos_vue-react/react-router-crud/src/components/AgregarProducto.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react'
+import Error from './Error'
 
 function AgregarProducto() {
 
     // state
     const [nombrePlatillo, guardarNombre] = useState('');    
     const [precioPlatillo, guardarPrecio] = useState('');
-    const [categoria, guardarCategoria] = useState('');    
+    const [categoria, guardarCategoria] = useState('');
+    const [error, guardarError] = useState(false);    
 
     const leerValorRadio = e => {
         guardarCategoria(e.target.value);
     }    
 
+    const agregarProducto = async e => {
+        e.preventDefault();
+
+        if (nombrePlatillo === '' || precioPlatillo === '' || categoria === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // Crear el nuevo producto
+
+    }
+
     return (
         <div className="col-md-8 mx-auto">
             <h1 className="text-center">Agregar Comida</h1>
 
-            <form className="mt-5" >
+            {(error) ? <Error mensaje='Todos los campos son obligatorios' /> : null}
+
+            <form className="mt-5" onSubmit={agregarProducto}>
                 <div className="form-group">
                     <label>Nombre</label>
                     <input

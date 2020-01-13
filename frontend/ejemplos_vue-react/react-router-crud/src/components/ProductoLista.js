663b6c8260f2c91d71178f2aad4ef7659fb79b91
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-12 19:41:33 -0600

    ej react app
    mostrar productos botones listado

--- frontend/ejemplos_vue-react/react-router-crud/src/components/ProductoLista.js
+++ frontend/ejemplos_vue-react/react-router-crud/src/components/ProductoLista.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ProductoLista({ producto }) {
 
+    const eliminarProducto = id => {
+        console.log('eliminando', id);
+
+        // TODO: Eliminar los registros
+    }
+
     return (
         <li data-categoria={producto.categoria} className="list-group-item d-flex justify-content-between align-items-center">
-            <p >
+            <p>
                 {producto.nombrePlatillo} {' '}
                 <span className="font-weight-bold">${producto.precioPlatillo}</span>
             </p>
+
+            <div>
+                <Link
+                    to={`/productos/editar/${producto.id}`}
+                    className="btn btn-success mr-2"
+                >Editar </Link>
+                <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => eliminarProducto(producto.id)}
+                >Eliminar &times;</button>                
+            </div>
         </li>
     )
 }

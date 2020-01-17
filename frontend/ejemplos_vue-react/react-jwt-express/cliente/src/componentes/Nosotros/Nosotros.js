d7a9640527d5382b4009422bfe1fc31faaba4972
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-17 11:54:31 -0600

    ej react app
    personalizacion base

--- frontend/ejemplos_vue-react/react-jwt-express/cliente/src/componentes/Nosotros/Nosotros.js
+++ frontend/ejemplos_vue-react/react-jwt-express/cliente/src/componentes/Nosotros/Nosotros.js
@@ -7,13 +7,17 @@ class Nosotros extends Component {
           return (
                <div className="contenedor-nosotros">
                     <div className="imagen">
-                         <img src="/img/camisa_1.png" alt="imagen nosotros" />
+                         <img src="/img/camisa_8.png" alt="imagen nosotros" />
                     </div>
                     <div className="contenido">
                          <h2>Sobre Nosotros</h2>
                          <p>Aenean auctor, augue id porta viverra, dui odio vehicula sem, eget imperdiet est tellus quis massa. Pellentesque tempor ac mauris id elementum. Donec luctus mauris at arcu semper, et luctus quam sagittis. Curabitur arcu lectus, porta et vulputate eget, ullamcorper non est. Praesent enim lectus, dictum ut scelerisque in, fringilla quis velit. Phasellus mattis ultricies sem, vitae egestas metus fermentum eget. Suspendisse pretium viverra magna, id tristique ipsum ornare ut.</p>
-                    </div>
+                         <div className="text-center" >
+                              <img src="/img/hero.jpg" alt="imagen nosotros" width="380" height="255"/>
+                         </div>
+                    </div>                    
                </div>
+
 
            )
      }

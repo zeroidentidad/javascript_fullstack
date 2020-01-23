4498b12f1b610a588d0cc2811161f0692ae5198f
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-22 22:41:09 -0600

    rn in action
    dimensions example

--- mobile/aRNinAction/apiscomunuesapp/App.js
+++ mobile/aRNinAction/apiscomunuesapp/App.js
@@ -3,11 +3,12 @@ import AlertExample from './src/AlertExample'
 import AppStateExample from './src/AppStateExample'
 import AsyncStorageExample from './src/AsyncStorageExample'
 import ClipboardExample from './src/ClipboardExample'
+import DimensionsExample from './src/DimensionsExample'
 
 
 const App: () => React$Node = () => {
   return (
-    <ClipboardExample />
+    <DimensionsExample />
   );
 };
 

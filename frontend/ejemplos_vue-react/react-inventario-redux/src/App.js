9c28970c19ad0eb90474254c0edb0cc3c04b944b
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-24 14:38:56 -0600

    ej react app
    ajustes iniciales

--- frontend/ejemplos_vue-react/react-inventario-redux/src/App.js
+++ frontend/ejemplos_vue-react/react-inventario-redux/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1 className="text-center">Inventario personal (web localStorage)</h1>
       </header>
+      <div className="row mt-2">
+        <div className="col-md-6">
+          Formulario
+        </div>
+        <div className="col-md-6">
+          Listado
+        </div>
+      </div>
     </div>
   );
 }

c05768f9a3bd20fd2be1132a897e35d1b62e95fd
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-25 14:54:49 -0600

    rn in action
    keyboard handle evetents example

--- mobile/aRNinAction/apiscomunuesapp/App.js
+++ mobile/aRNinAction/apiscomunuesapp/App.js
@@ -5,11 +5,12 @@ import AsyncStorageExample from './src/AsyncStorageExample'
 import ClipboardExample from './src/ClipboardExample'
 import DimensionsExample from './src/DimensionsExample'
 import GeolocationExample from './src/GeolocationExample'
+import KeyboardExample from './src/KeyboardExample'
 
 
 const App: () => React$Node = () => {
   return (
-    <GeolocationExample />
+    <KeyboardExample />
   );
 };
 

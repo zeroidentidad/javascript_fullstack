a5fbb48591b230a98649231241ba32c8bfab3938
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-24 23:11:56 -0600

    ej react app
    eliminar articulos

--- frontend/ejemplos_vue-react/react-inventario-redux/src/components/ListadoArticulos.js
+++ frontend/ejemplos_vue-react/react-inventario-redux/src/components/ListadoArticulos.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
+import { borrarArticuloAction } from '../redux/actions/articulosActions';
 
 const ListadoArticulos = () => {
 
     // obtener los articulos del state
     const articulos = useSelector((state) => state.articulos);  
     
+    // Dispatch para mandar llamar la acción de eliminar
+    const dispatch = useDispatch();    
     
     // Mensaje condicional
     const mensaje = Object.keys(articulos.articulos).length === 0 ? 'No hay articulos' : 'Articulos en localStorage';    
@@ -17,15 +20,18 @@ const ListadoArticulos = () => {
 
                 <div className="lista-articulos">
                     {articulos.articulos.map(articulo => (
-                    <div className="media mt-1">
+                    <div key={articulo.id} className="media mt-1">
                         <div className="media-body">
                             <h3 className="mt-0"></h3>
-                            <p className="card-text"><span>ID:</span> {articulo.id}</p>
+                            <p className="card-text"><span>ID:</span> {articulo.id.substring(0, 10)}</p>
                             <p className="card-text"><span>Nombre:</span> {articulo.articulo}</p>
                             <p className="card-text"><span>Fecha existencia:</span> {articulo.fecha}</p>
                             <p className="card-text"><span>Area de uso:</span> {articulo.area} <span>Fabricante/modelo:</span> {articulo.modelo}</p>
                             <p className="card-text"><span>Descripción:</span> <br/> {articulo.descripcion}</p>
-                            <button className="btn btn-danger">Borrar &times;</button>
+                            <button className="btn btn-danger"
+                            onClick={() => dispatch(borrarArticuloAction(articulo.id))}
+                            >Borrar &times;
+                            </button>
                         </div>
                     </div>
                     ))}
--- frontend/ejemplos_vue-react/react-inventario-redux/src/redux/reducers/articulosReducer.js
+++ frontend/ejemplos_vue-react/react-inventario-redux/src/redux/reducers/articulosReducer.js
@@ -9,6 +9,11 @@ export default function (state = initialState, action) {
                 ...state,
                 articulos: [...state.articulos, action.payload]
             }
+        case 'BORRAR_ARTICULO':
+            return {
+                ...state,
+                articulos: state.articulos.filter(articulo => articulo.id !== action.payload)
+            }
         default:
             return state;
     }

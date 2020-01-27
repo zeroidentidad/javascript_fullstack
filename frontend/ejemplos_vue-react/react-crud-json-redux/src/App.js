1fae4fdfb59df13fefad51703b8556868a736fea
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-26 22:50:34 -0600

    ej react app
    init routing

--- frontend/ejemplos_vue-react/react-crud-json-redux/src/App.js
+++ frontend/ejemplos_vue-react/react-crud-json-redux/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Browser as Router, Route, Switch} from 'react-router-dom'
 // Redux
 import { Provider } from 'react-redux'
 import store from './store'
 
 function App() {
   return (
+    <Router>
     <Provider store={store}>
-        <p>CRUD Redux</p>
+      <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Publicaciones} />
+            <Route exact path="/publicaciones/nueva" component={NuevaPublicacion} />
+            <Route exact path="/publicaciones/editar/:id" component={EditarPublicacion} />
+          </Switch>
+        </div>
     </Provider>
+    </Router>
   );
 }
 

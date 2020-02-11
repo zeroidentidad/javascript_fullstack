ac20610df61d9a9d167153e6a750d11f121f7f66
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-10 20:29:40 -0600

    ej react app
    config y conexion firestore

--- frontend/ejemplos_vue-react/react-libros-firestore-redux/src/App.js
+++ frontend/ejemplos_vue-react/react-libros-firestore-redux/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// redux
+import store from './store'
+import {Provider} from 'react-redux'
 // componentes
 import Navbar from './components/layout/Navbar'
 import Suscriptores from './components/suscriptores/Suscriptores'
@@ -9,6 +12,7 @@ import EditarSuscriptor from './components/suscriptores/EditarSuscriptor'
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Navbar />
       <div className="container">
@@ -20,6 +24,7 @@ function App() {
         </Switch>
       </div>
     </Router>
+    </Provider>
   );
 }
 
---
+++ frontend/ejemplos_vue-react/react-libros-firestore-redux/src/store.js
@@ -0,0 +1,48 @@
+import {createStore, combineReducers, compose} from 'redux'
+import {reactReduxFirebase, firebaseReducer} from 'react-redux-firebase'
+import {reduxFirestore, firestoreReducer} from 'redux-firestore'
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+
+//config firestore
+const firebaseConfig =  {
+    apiKey: "AIzaSyD_a05cwyrhb42i69BwMWRvQAChQ2FryUw",
+    authDomain: "libros-store.firebaseapp.com",
+    databaseURL: "https://libros-store.firebaseio.com",
+    projectId: "libros-store",
+    storageBucket: "libros-store.appspot.com",
+    messagingSenderId: "646994303453",
+    appId: "1:646994303453:web:d6302826324337ba4dbe34"    
+}
+
+//inicializar firebase
+firebase.initializeApp(firebaseConfig)
+
+//config react-redux
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true
+}
+
+// crear el enhacer con compose de redux y firestore
+const createStoreWithFirebase = compose(
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase)
+)(createStore);
+
+// Reducers 
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
+})
+
+// state inicial
+const initialState = {};
+
+// Create el store
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;

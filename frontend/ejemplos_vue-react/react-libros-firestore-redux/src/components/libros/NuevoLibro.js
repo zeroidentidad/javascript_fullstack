33bc6f1f2e6f8f861499772fd0710683b5e66287
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-15 15:12:58 -0600

    ej react app
    agregar libro a firestore

--- frontend/ejemplos_vue-react/react-libros-firestore-redux/src/components/libros/NuevoLibro.js
+++ frontend/ejemplos_vue-react/react-libros-firestore-redux/src/components/libros/NuevoLibro.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import { firestoreConnect } from 'react-redux-firebase'
+import PropTypes from 'prop-types'
 
 class NuevoLibro extends Component {
+    
     state = {
         titulo: '',
         isbn: '',
         editorial: '',
         existencia: ''
     }
+
+    agregarLibro = e => {
+        e.preventDefault()
+
+        // extraer valores del state
+        const nuevoLibro = { ...this.state }
+
+        //agregar array prestados
+        nuevoLibro.prestados = []
+
+        // utilizar firestore de props
+        const { firestore, history } = this.props
+
+        // guardar en db
+        firestore.add({ collection: 'libros' }, nuevoLibro
+        ).then(() => (history.push('/')))
+
+    }    
+
+    // datos del form al state
+    leerDato = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className="row">
@@ -22,7 +51,7 @@ class NuevoLibro extends Component {
                     </h2>
                     <div className="row justify-content-center">
                         <div className="col-md-8 mt-5">
-                            <form>
+                            <form onSubmit={this.agregarLibro} >
                                 <div className="form-group">
                                     <label htmlFor="titulo">Titulo:</label>
                                     <input 
@@ -83,4 +112,8 @@ class NuevoLibro extends Component {
     }
 }
 
-export default NuevoLibro
+NuevoLibro.propTypes = {
+    firestore: PropTypes.object.isRequired
+}
+
+export default firestoreConnect()(NuevoLibro)

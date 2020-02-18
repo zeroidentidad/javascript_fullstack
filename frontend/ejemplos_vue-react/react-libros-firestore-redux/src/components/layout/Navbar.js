f2c64a9f50c75891d00e996a4f75e6760572aa07
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-18 11:06:10 -0600

    ej react app
    manejo estado de autentcación

--- frontend/ejemplos_vue-react/react-libros-firestore-redux/src/components/layout/Navbar.js
+++ frontend/ejemplos_vue-react/react-libros-firestore-redux/src/components/layout/Navbar.js
@@ -1,29 +1,63 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firebaseConnect } from 'react-redux-firebase'
+import PropTypes from 'prop-types'
 
-const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
-            <nav className="navbar navbar-light">
-                <span className="navbar-brand mb-0 h1">Admin </span>
-                <h2><i className="fas fa-university"></i></h2>
+class Navbar extends Component {
+
+    state = {
+        usuarioAutenticado: false
+    }    
+
+    // recibir props automaticamente
+    static getDerivedStateFromProps(props, state) {
+        const { auth } = props;
+
+        if (auth.uid) {
+            return { usuarioAutenticado: true }
+        } else {
+            return { usuarioAutenticado: false }
+        }
+    }
+
+    render(){
+
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+                <nav className="navbar navbar-light">
+                    <span className="navbar-brand mb-0 h1">Admin </span>
+                    <h2><i className="fas fa-university"></i></h2>
+                </nav>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarColor03">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={'/'} className="nav-link">Libros</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={'/suscriptores'} className="nav-link">Suscriptores</Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        )
 
-            <div className="collapse navbar-collapse" id="navbarColor03">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to={'/'} className="nav-link">Libros</Link>
-                    </li> 
-                    <li className="nav-item">
-                        <Link to={'/suscriptores'} className="nav-link">Suscriptores</Link>
-                    </li>                                       
-                </ul>
-            </div>
-        </nav>
-    )
+    }
 }
 
-export default Navbar
+Navbar.propTypes = {
+    firebase: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+export default compose(
+    firebaseConnect(),
+    connect((state, props) => ({
+        auth: state.firebase.auth
+    }))
+)(Navbar);

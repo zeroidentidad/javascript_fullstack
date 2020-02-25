fa7ae7676203087478ff19acea15a9d37a14d591
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-24 18:39:59 -0600

    react facilito
    intro ciclo state con useffet

--- frontend/ejemplos_react-facilito/react-pro-actual/ej-react-state/src/App.js
+++ frontend/ejemplos_react-facilito/react-pro-actual/ej-react-state/src/App.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Button =()=>{
+
   const [count, setCount]= useState(0)
+
+  useEffect(() => {
+    if (count>0) {
+      alert('sumando prro')
+    }
+    return()=>{
+      console.log("Adios")
+    }
+  }, [count])
+
   return (
     <div>
       <p>Presionado: {count}</p>

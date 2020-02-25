9470cc3b27ee24d179885b886245c9b8e1bae272
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-24 20:54:22 -0600

    react facilito
    enviar formulario

--- frontend/ejemplos_react-facilito/react-pro-actual/ej-react-forms/src/App.js
+++ frontend/ejemplos_react-facilito/react-pro-actual/ej-react-forms/src/App.js
@@ -5,21 +5,42 @@ const Form = () => {
   let [titulo, setTitulo] = useState('')
   let [cuerpo, setCuerpo] = useState('')
 
+  const sendForm = (e) => {
+    e.preventDefault()
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: titulo,
+        body: cuerpo,
+        userId: 1
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setTitulo("")
+        setCuerpo("")
+      })
+
+  }
+
   return(
-    <form >
+    <form onSubmit={(e)=>sendForm(e)}>
       <div>
         <label htmlFor="titulo">Titulo </label>
-        <input type="text" id="titulo" onChange={(e) => setTitulo(e.target.value)}/>
+        <input type="text" id="titulo" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
       </div>  
 
       <div>
         <label htmlFor="cuerpo">Cuerpo </label>
-        <textarea id="cuerpo" onChange={(e) => setCuerpo(e.target.value)}></textarea>
+        <textarea id="cuerpo" value={cuerpo} onChange={(e) => setCuerpo(e.target.value)}></textarea>
       </div>
 
         <input type="submit" value="Enviar"/>
-
-        <p>Test: {titulo} {cuerpo}</p>
     </form>
   )
 }

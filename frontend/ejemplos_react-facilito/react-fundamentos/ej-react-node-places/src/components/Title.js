180e954cbcfb04c85ce126be4a54dab9c4f6b327
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-03-04 17:57:21 -0600

    react facilito
    init component

---
+++ frontend/ejemplos_react-facilito/react-fundamentos/ej-react-node-places/src/components/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Places</h1>
+                <p>Descubrir lugares de manera simple</p>
+            </div>
+        )
+    }
+}
+
+export default Title

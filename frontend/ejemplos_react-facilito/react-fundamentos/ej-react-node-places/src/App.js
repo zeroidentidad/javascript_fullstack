0cc9d1ab61000bbba86a7894a8ac8fd79182cfd7
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-03-08 15:02:44 -0600

    react facilito
    test classic state

--- frontend/ejemplos_react-facilito/react-fundamentos/ej-react-node-places/src/App.js
+++ frontend/ejemplos_react-facilito/react-fundamentos/ej-react-node-places/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Title from './components/Title';
 import Pin from './images/top_background.png'
 
-function App() {
-  return (
-    <section>
-      <div>
+class App extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      numero: 0
+    }
+
+    setInterval(() => {
+      this.setState({ numero: this.state.numero + 1 })
+    }, 1000);     
+  } 
+
+  render(){
+    return (
+      <section>
         <div>
-          <Title />
-          <button>Crear cuenta</button>
-          <img src={Pin} alt="Pin" height="200"/>
           <div>
-            <ul>
-              <li><h3>Calificar lugares</h3></li>
-              <li><h3>Experiencia offline o conexion lenta</h3></li>
-              <li><h3>Coleccion favotiros</h3></li>
-            </ul>
+            <Title />
+            <h3>{this.state.numero}</h3>
+            <button>Crear cuenta</button>
+            <img src={Pin} alt="Pin" height="200" />
+            <div>
+              <ul>
+                <li><h3>Calificar lugares</h3></li>
+                <li><h3>Experiencia offline o conexion lenta</h3></li>
+                <li><h3>Coleccion favotiros</h3></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
+
 }
 
 export default App;

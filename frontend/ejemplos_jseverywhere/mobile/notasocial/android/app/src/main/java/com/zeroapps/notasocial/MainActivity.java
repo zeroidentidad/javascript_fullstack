1e732a0697cb4a3206981bc28083aff266de8b24
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-04-01 13:07:04 -0600

    jseverywhere
    rename and test run app

--- frontend/ejemplos_jseverywhere/mobile/notasocial/android/app/src/main/java/com/zeroapps/notasocial/MainActivity.java
+++ frontend/ejemplos_jseverywhere/mobile/notasocial/android/app/src/main/java/com/zeroapps/notasocial/MainActivity.java
@@ -1,4 +1,4 @@
-package com.notasocial;
+package com.zeroapps.notasocial;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "notasocial";
+    return "Notasocial";
   }
 }

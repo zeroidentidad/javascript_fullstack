062c44cea0968345b9b6c897a8df003ad00b91da
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-04-07 22:21:48 -0500

    react facilito
    router

--- frontend/ejemplos_react-facilito/react-fundamentos/ej-react-node-places/src/App.js
+++ frontend/ejemplos_react-facilito/react-fundamentos/ej-react-node-places/src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css'
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Home from './pages/Home'
 
 const theme = createMuiTheme({})
 
@@ -10,7 +9,7 @@ class App extends Component{
   render(){
     return (
       <ThemeProvider theme={theme}>
-          <Home />         
+        {this.props.children}
       </ThemeProvider>
     );
   }
---
+++ frontend/ejemplos_react-facilito/react-fundamentos/ej-react-node-places/src/Router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as ReactRouter, Route } from 'react-router-dom';
+import App from './App'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+export default class Router extends React.Component {
+    render(){
+        return(
+        <ReactRouter>
+            <App>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+            </App>
+        </ReactRouter>
+        )
+    }
+}

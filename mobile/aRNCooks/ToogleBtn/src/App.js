d9a3cd57fca537045c33f4d72ab62147788d3a7e
zeroidentidad <jafs-2050@hotmail.com>
2020-06-23 18:18:15 -0500

    rncooks togglebtn

---
+++ mobile/aRNCooks/ToogleBtn/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
+
+const heartIcon=require('./images/heart.png');
+
+export default class App extends React.Component {
+  state={
+    liked: false,
+  };
+  handleButtonPress=() => {
+    this.setState({
+      liked: !this.state.liked,
+    });
+  }
+  render() {
+    const likedStyles=this.state.liked? styles.liked:undefined;
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight
+          onPress={this.handleButtonPress}
+          style={styles.button}
+          underlayColor="#fefefe"
+        >
+          <Image
+            source={heartIcon}
+            style={[styles.icon, likedStyles]}
+          />
+        </TouchableHighlight>
+        <Text style={styles.text}>Do you like this app?</Text>
+      </View>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  container: {
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  button: {
+    borderRadius: 5,
+    padding: 10,
+  },
+  icon: {
+    width: 180,
+    height: 180,
+    tintColor: '#f1f1f1',
+  },
+  liked: {
+    tintColor: '#e74c3c',
+  },
+  text: {
+    marginTop: 20,
+  },
+});

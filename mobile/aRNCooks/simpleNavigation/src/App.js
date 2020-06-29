8051aa7b366c923c9a9a2812cdeba794ba535679
zeroidentidad <jafs-2050@hotmail.com>
2020-06-29 15:54:08 -0500

    rncooks simpleNav

---
+++ mobile/aRNCooks/simpleNavigation/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MainTabNavigator from './nav/MainTabNavigator';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MainTabNavigator />
+      </View>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
---
+++ mobile/aRNCooks/simpleNavigation/src/screens/LinksScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+export default class LinksScreen extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.headline}>
+                    Links
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F8759D',
+    },
+    headline: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        color: 'white',
+    }
+});

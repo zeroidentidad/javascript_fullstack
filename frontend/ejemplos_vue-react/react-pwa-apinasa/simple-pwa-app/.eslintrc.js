87e572985dbb3da8e4086f63e6de387eb39493bf
zeroidentidad <jafs-2050@hotmail.com>
2020-07-06 15:00:43 -0500

    pwa eslint test dev sublime

---
+++ frontend/ejemplos_vue-react/react-pwa-apinasa/simple-pwa-app/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    }
+    "rules": {
+    }
+};

bf4b90577fa213875fb5c5221dc2735a876e04d4
zeroidentidad <jafs-2050@hotmail.com>
2020-10-05 20:59:14 -0500

    nodejs practice patron creacional fabric

--- backend/x_sistema_ojala/src/vuelos/methods.js
+++ backend/x_sistema_ojala/src/vuelos/methods.js
@@ -1,32 +1,37 @@
 
-module.exports = info => {
-    let valores = {
+const vuelo = function(){
+    this.data={
         numero: null,
         origen: null,
         destino: null,
         fechaPartida: null,
         fechaLlegada: null,
         actualFechaPartida: null,
-        atualFechaLlegada: null
-    } 
+        atualFechaLlegada: null        
+    };
+
+    this.registrar= info =>{
+        for (let prop in this.data) {
+            if (this.data[prop]!=typeof (undefined)) {
+                this.data[prop]=info[prop];
+            }
+        }
+    }
+
+    this.triggerPartida=()=>{
+        this.data.actualFechaPartida=Date.now();
+    }
+    this.triggerLlegada=()=>{
+        this.data.atualFechaLlegada=Date.now();
+    }
+    this.getInfo=()=>{
+        return this.data;
+    }
+}
+
+module.exports = info => {
+    const instanciaVuelo = new vuelo();
+    instanciaVuelo.registrar(info);
     
-    for (let prop in valores) {
-        if(valores[prop]!=typeof(undefined)){
-            valores[prop]=info[prop];
-        }
-    }
-
-    let funciones = {
-        triggerPartida: function () {
-            valores.actualFechaPartida=Date.now();
-        },
-        triggerLlegada: function () {
-            valores.atualFechaLlegada=Date.now();
-        },
-        getInfo: function () {
-            return valores;
-        }
-    }
-
-    return funciones;
+    return instanciaVuelo;
 }

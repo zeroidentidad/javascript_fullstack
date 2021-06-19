b1a25006ea7e8835edcf23ba347fda9e5f8ff1af
zeroidentidad <jafs-2050@hotmail.com>
2021-06-18 23:43:35 -0500

    ember tuts avance

---
+++ frontend/ejemplos_emberjs/todo-mvc/app/services/todo-data.js
@@ -0,0 +1,27 @@
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+class Todo {
+	@tracked text = '';
+	@tracked isCompleted = false;
+
+	constructor(text) {
+		this.text = text;
+	}
+}
+
+export default class TodoDataService extends Service {
+	@tracked todos = [];
+
+	get all() {
+		return this.todos;
+	}  
+
+	@action
+	add(text) {
+		let newTodo = new Todo(text);
+
+		this.todos = [...this.todos, newTodo];
+	}	
+}

a7d3a8d74e5d390f6370be1255cd63e79c44214a
zeroidentidad <jafs-2050@hotmail.com>
2021-06-28 11:10:41 -0500

    ember tuts avance

--- frontend/ejemplos_emberjs/rock-ember/app/controllers/bands.js
+++ frontend/ejemplos_emberjs/rock-ember/app/controllers/bands.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import Band from 'rock-ember/models/band';
+import { empty } from '@ember/object/computed';
 
 export default class BandsController extends Controller {
-    isAddingBand=false
-    newBandName=''
+    isAddingBand=false;
+    newBandName='';
+    isAddButtonDisabled=empty('newBandName');
 
     @action
     addBand() {
@@ -17,6 +20,8 @@ export default class BandsController extends Controller {
 
     @action
     saveBand() {
-        // Create a new band
+        let newBand=Band.create({ name: this.newBandName });
+        this.model.pushObject(newBand);
+        this.set('newBandName', '');
     }
 }
--- frontend/ejemplos_emberjs/rock-ember/app/routes/bands.js
+++ frontend/ejemplos_emberjs/rock-ember/app/routes/bands.js
@@ -1,20 +1,7 @@
 import Route from '@ember/routing/route';
 import { A } from '@ember/array';
-import EmberObject, { computed } from '@ember/object';
-import { dasherize } from '@ember/string';
-
-let Band = EmberObject.extend({
-	name: '',
-	slug: computed('name', function() {
-		return dasherize(this.name);
-	})	
-});
-
-let Song = EmberObject.extend({
-	title: '',
-	rating: 0,
-	band: ''
-});
+import Band from 'rock-ember/models/band';
+import Song from 'rock-ember/models/song';
 
 export default class BandsRoute extends Route {
 	model() {

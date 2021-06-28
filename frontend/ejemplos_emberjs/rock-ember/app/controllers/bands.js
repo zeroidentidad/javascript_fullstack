3aa56f9eada8dccb4736f4c075e0d04da6f231c2
zeroidentidad <jafs-2050@hotmail.com>
2021-06-28 14:33:51 -0500

    ember tuts avance

--- frontend/ejemplos_emberjs/rock-ember/app/controllers/bands.js
+++ frontend/ejemplos_emberjs/rock-ember/app/controllers/bands.js
@@ -1,27 +1,29 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import Band from 'rock-ember/models/band';
 import { empty } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
-export default class BandsController extends Controller {
-    isAddingBand=false;
-    newBandName='';
-    isAddButtonDisabled=empty('newBandName');
-
-    @action
-    addBand() {
-        this.set('isAddingBand', true);
+export default Controller.extend({
+    isAddingBand: false,
+    newBandName: '',
+    isAddButtonDisabled: empty('newBandName'),
+    router: service(),
+    actions: {
+        addBand() {
+            this.set('isAddingBand', true);
+        },
+        cancelAddBand() {
+            this.set('isAddingBand', false);
+        },
+        saveBand(event) {
+            event.preventDefault();
+            let newBand=Band.create({ name: this.newBandName });
+            this.model.pushObject(newBand);
+            this.setProperties({
+                newBandName: '',
+                isAddingBand: false
+            });
+            this.router.transitionTo('bands.band.songs', newBand);
+        }
     }
-
-    @action
-    cancelAddBand() {
-        this.set('isAddingBand', false);
-    }
-
-    @action
-    saveBand() {
-        let newBand=Band.create({ name: this.newBandName });
-        this.model.pushObject(newBand);
-        this.set('newBandName', '');
-    }
-}
+});
---
+++ frontend/ejemplos_emberjs/rock-ember/app/controllers/bands/band/songs.js
@@ -0,0 +1,23 @@
+import Controller from '@ember/controller';
+import Song from 'rock-ember/models/song';
+import { empty } from '@ember/object/computed';
+
+export default Controller.extend({
+    isAddingSong: false,
+    newSongTitle: '',
+    isAddButtonDisabled: empty('newSongTitle'),
+    actions: {
+        addSong() {
+            this.set('isAddingSong', true);
+        },
+        cancelAddSong() {
+            this.set('isAddingSong', false);
+        },
+        saveSong(event) {
+            event.preventDefault();
+            let newSong=Song.create({ title: this.newSongTitle });
+            this.model.songs.pushObject(newSong);
+            this.set('newSongTitle', '');
+        }
+    }
+});
